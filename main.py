f256ffb894f5a898ce6448f40603016d80055b14
linshihang <linshihang@modo.cn>
2025-04-16 18:12:01 +0800

    1

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from utils.config_loader import config
+from core.adb_controller import ADBController
 
 def main():
     # 获取配置值
-    adb_path = config.get('emulator', 'adb_path')
-    port = config.get('emulator', 'port')
-    package_name = config.get('game', 'package_name')
+    # adb_path = config.get('emulator', 'adb_path')
+    # port = config.get('emulator', 'port')
+    # package_name = config.get('game', 'package_name')
     
-    print(f"ADB路径: {adb_path}")
-    print(f"模拟器端口: {port}")
-    print(f"游戏包名: {package_name}")
+    # print(f"ADB路径: {adb_path}")
+    # print(f"模拟器端口: {port}")
+    # print(f"游戏包名: {package_name}")
     
-    # 获取整个emulator配置节
-    emulator_config = config.get('emulator')
-    print(f"完整模拟器配置: {emulator_config}")
+    # # 获取整个emulator配置节
+    # emulator_config = config.get('emulator')
+    # print(f"完整模拟器配置: {emulator_config}")
+    adb = ADBController(adb_path=config.get('emulator', 'adb_path'), device_serial=config.get('emulator', 'mumu_serial'))
+
 
 if __name__ == "__main__":
     main()
